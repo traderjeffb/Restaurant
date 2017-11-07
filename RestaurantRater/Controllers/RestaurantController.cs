7cd509e5f0460d6277e47b3af66ef363bbeaba57
Jeff Beeson <traderjeffb@gmail.com>
2017-11-07 11:07:33 -0500

    Restaurant

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantRater.Models;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDBContext db = new RestaurantDBContext();
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(db.Restaurants.ToList());
+        }
+
+        //GET : Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestaurantID,Name")]Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+    }
+}
